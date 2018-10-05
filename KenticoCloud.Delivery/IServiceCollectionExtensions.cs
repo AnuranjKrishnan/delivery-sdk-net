2d022697b7e9412c17f1431ecb802bceb7afdb6a
Matus Backor <MatusB@kentico.com>
2018-10-05 15:29:41 +0200

    DRAFT-4768 Add extension method for IServiceCollection

---
+++ KenticoCloud.Delivery/IServiceCollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using KenticoCloud.Delivery.InlineContentItems;
+using KenticoCloud.Delivery.ResiliencePolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace KenticoCloud.Delivery
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDeliveryClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOptions();
+            services.TryAddTransient(serviceProvider => (IContentLinkUrlResolver)null);
+            services.TryAddTransient(serviceProvider => (ICodeFirstTypeProvider)null);
+            services.TryAddTransient<IInlineContentItemsResolver<object>, ReplaceWithWarningAboutRegistrationResolver>();
+            services.TryAddTransient<IInlineContentItemsResolver<UnretrievedContentItem>, ReplaceWithWarningAboutUnretrievedItemResolver>();
+            services.TryAddTransient<IInlineContentItemsProcessor, InlineContentItemsProcessor>();
+            services.TryAddTransient<ICodeFirstModelProvider, CodeFirstModelProvider>();
+            services.TryAddTransient<ICodeFirstPropertyMapper, CodeFirstPropertyMapper>();
+
+            services.TryAddTransient<IResiliencePolicyProvider>(serviceProvider =>
+            {
+                var deliveryOptions = serviceProvider.GetService<IOptions<DeliveryOptions>>();
+
+                return new DefaultResiliencePolicyProvider(deliveryOptions.Value.MaxRetryAttempts);
+            });
+
+            services.TryAddTransient<IDeliveryClient>(serviceProvider =>
+            {
+                var deliveryOptions = serviceProvider.GetService<IOptions<DeliveryOptions>>();
+                var contentLinkUrlResolver = serviceProvider.GetService<IContentLinkUrlResolver>();
+                var inlineContentItemsProcessor = serviceProvider.GetService<IInlineContentItemsProcessor>();
+                var codeFirstModelProvider = serviceProvider.GetService<ICodeFirstModelProvider>();
+                var resiliencePolicyProvider = serviceProvider.GetService<IResiliencePolicyProvider>();
+                var codeFirstTypeProvider = serviceProvider.GetService<ICodeFirstTypeProvider>();
+                var codeFirstPropertyMapper = serviceProvider.GetService<ICodeFirstPropertyMapper>();
+
+                return new DeliveryClient(
+                    deliveryOptions,
+                    contentLinkUrlResolver,
+                    inlineContentItemsProcessor,
+                    codeFirstModelProvider,
+                    resiliencePolicyProvider,
+                    codeFirstTypeProvider,
+                    codeFirstPropertyMapper
+                );
+            });
+
+            return services;
+        }
+    }
+}
